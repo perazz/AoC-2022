f2e1ed80b5facc9ba879e10bd2b84c8ec3ed6214
Federico Perini <federico.perini@gmail.com>
2022-12-02 18:49:34 +0100

    01 shortest form possible

--- 01/project/main.f90
+++ 01/project/main.f90
@@ -1,23 +1,9 @@
-program aoc_2022_01
-    use iso_fortran_env
-    implicit none
-
-    character(*), parameter :: fileName = 'input.txt'
-    integer :: iunit,ios,meal
-    integer, allocatable :: calories(:)
-
-    open(newunit=iunit,file=fileName,form='formatted',iostat=ios)
-    allocate(calories(1),source=0)
-    do while (.not.is_iostat_end(ios))
-        read(iunit,'(i10)',blank='ZERO',iostat=ios) meal
-        if (meal==0) then
-            calories = [calories,meal]
-        else
-            calories(size(calories)) = calories(size(calories))+meal
-        end if
-    end do
-    close(iunit)
-
-    print "(*(a,:,i0))", 'elf ',maxloc(calories),' of ',size(calories),' ate ',maxval(calories),' calories'
-
-end program
+1 integer, allocatable :: eat(:)
+2 open(newunit=iunit,file='input.txt',form='formatted')
+3 allocate(eat(1),source=0)
+4 read(iunit,'(i10)',blank='ZERO',err=8,end=8) meal
+5 if (meal) 6,6,7 
+6 eat = [eat,meal]; goto 4
+7 eat(size(eat)) = eat(size(eat))+meal; goto 4
+8 print "(*(a,:,i0))", 'elf ',maxloc(eat),' of ',size(eat),' ate ',maxval(eat),' calories'
+9 end program
